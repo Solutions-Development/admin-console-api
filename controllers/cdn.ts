fd4609f4ab2b96bb3399ba2c34e0c6b0bb28facd
Jose Barranco <jdcoding01@gmail.com>
2020-02-26 19:28:06 -0400

    @

--- controllers/cdn.ts
+++ controllers/cdn.ts
@@ -1,5 +1,5 @@
 const path = require("path");
 
 export async function banner(req: any, reply: any) {
-    reply.sendFile(path.resolve('../webapp-api/cdn/gastrobanner.jpeg'));
+    reply.sendFile(path.resolve('../admin-console-api/cdn/gastrobanner.jpeg'));
 }
